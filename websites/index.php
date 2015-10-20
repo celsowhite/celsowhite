93ceb5049fe3162884fb2f28bbf501ba2ac2b980
celsowhite <celso.white13@gmail.com>
2015-10-20 08:19:47 -0400

    Update websites page layout to use flexbox styling.

--- websites/index.php
+++ websites/index.php
@@ -57,152 +57,83 @@
     </div>
   </section>
 
-  <div class="row">
+  <ul class="website_blocks">
 
-    <div class="six columns">
-      <div class="portfolio">
-       <p class="title">Welcome.US | <span class="descriptor">Non-Profit</span></p>
-       <a href="http://welcome.us/" target="_blank"><img src="../images/welcomeus.png"></a>
-       <p class="portfolio-description">Welcome US is the official site for Immigrant Heritage Month. The site tells the immigration stories of Americans. I was the developer on the project. Building the functionality to compile both curated stories and stories shared through social media and the ability for people to share their own stories. Project lead and designed by <a href="http://verynice.co/" target="_blank">verynice.</a>
-       </br>
-       <a href="http://welcome.us" target="_blank">View Welcome.US</a>
-       </p>
-      </div>
-    </div>
+    <li>
+        <p class="title">Welcome.US | <span class="descriptor">Non-Profit</span></p>
+        <a href="http://welcome.us/" target="_blank"><img src="../images/welcomeus.png"></a>
+        <div class="portfolio-description">
+          <p>Welcome US is the official site for Immigrant Heritage Month. The site tells the immigration stories of Americans. I was the developer on the project. Building the functionality to compile both curated stories and stories shared through social media and the ability for people to share their own stories. Project lead and designed by <a href="http://verynice.co/" target="_blank">verynice.</a></p>
+          <a href="http://welcome.us" target="_blank">View Welcome.US</a>
+        </div>
+    </li>
 
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">The Water Trust | <span class="descriptor">Non-Profit</span></p>
        <a href="https://watertrust.org/" target="_blank"><img src="../images/thewatertrust.png"></a>
-       <p class="portfolio-description">The Water Trust is a non-profit that creates water, sanitation and hygiene systems in East Africa. I designed and developed a website that helps the orginzation better connect with donors, shares real-time updates for their projects, and gives a vivid lens into the lives of the communities they effect.
-       </br>
-       <a href="https://watertrust.org/" target="_blank">View The Water Trust</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>The Water Trust is a non-profit that creates water, sanitation and hygiene systems in East Africa. I designed and developed a website that helps the orginzation better connect with donors, shares real-time updates for their projects, and gives a vivid lens into the lives of the communities they effect.</p>
+          <a href="https://watertrust.org/" target="_blank">View The Water Trust</a>
+       </div>
+    </li>
 
-  </div>
-
-  <div class="row">
-
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">PBS | <span class="descriptor">Media</span></p>
        <a href="http://americabythenumbers.org/" target="_blank"><img src="../images/abtn.png"></a>
-       <p class="portfolio-description">America by The Numbers is a new PBS documentary series. I helped create an online experience to share stories and facts from the show. The new website also reflects the unique character the series brings to PBS.
-       </br> 
-       </br> 
-       </br> 
-       <a href="http://americabythenumbers.org/" target="_blank">View America By The Numbers</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>America by The Numbers is a new PBS documentary series. I helped create an online experience to share stories and facts from the show. The new website also reflects the unique character the series brings to PBS.</p>
+          <a href="http://americabythenumbers.org/" target="_blank">View America By The Numbers</a>
+       </div>
+    </li>
 
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">HUMAN | <span class="descriptor">Agency</span></p>
        <a href="http://agency.thehumanstory.com/" target="_blank"><img src="../images/HUMAN.png"></a>
-       <p class="portfolio-description">HUMAN is a storytelling and strategy agency that creates incredible media pieces for social impact. I developed a site to showcase their work and refresh their digital presence. Capturing the essence of the agency and their new service offerings.
-       </br> 
-       </br>
+       <div class="portfolio-description">
+          <p>HUMAN is a storytelling and strategy agency that creates incredible media pieces for social impact. I developed a site to showcase their work and refresh their digital presence. Capturing the essence of the agency and their new service offerings.</p>
+          <a href="http://agency.thehumanstory.com/" target="_blank">View HUMAN</a>
+       </div>
+    </li>
 
-       <a href="http://agency.thehumanstory.com/" target="_blank">View HUMAN</a>
-       </p>
-      </div>
-    </div>
-
-  </div>
-
-   <div class="row">
-
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">Work Shoes | <span class="descriptor">E-commerce</span></p>
        <a href="http://workshoes.com/" target="_blank"><img src="../images/workshoes.png"></a>
-       <p class="portfolio-description">Work Shoes is an online shop for specialty shoes. Along with a team, I was in charge of improving their e-commerce experience. This included migrating their content to Shopify, improving the sites navigation, reorganizing products within a filter system and creating a more action driven landing page. I lead the user experience and Shopify development for the project.
-       </br> 
-       </br>
-       <a href="http://workshoes.com/" target="_blank">View Work Shoes</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>Work Shoes is an online shop for specialty shoes. Along with a team, I was in charge of improving their e-commerce experience. This included migrating their content to Shopify, improving the sites navigation, reorganizing products within a filter system and creating a more action driven landing page. I lead the user experience and Shopify development for the project.</p>
+          <a href="http://workshoes.com/" target="_blank">View Work Shoes</a>
+       </div>
+    </li>
 
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">Bold Organics | <span class="descriptor">Business</span></p>
        <a href="http://bold-organics.com/" target="_blank"><img src="../images/bold.jpg"></a>
-       <p class="portfolio-description">Bold Organics is one of the top producers of gluten-free pizza in the world. I helped refine and adapt their digital presence to include a new organic pizza line and attract new consumers.
-       </br> 
-       </br> 
-       </br>
-       </br> 
-       </br> 
-       <a href="http://bold-organics.com/" target="_blank">View Bold Organics</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>Bold Organics is one of the top producers of gluten-free pizza in the world. I helped refine and adapt their digital presence to include a new organic pizza line and attract new consumers.</p>
+          <a href="http://bold-organics.com/" target="_blank">View Bold Organics</a>
+       </div>
+    </li>
 
-  </div>
-
-  <div class="row">
-
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">Sylvio | <span class="descriptor">Fashion</span></p>
        <a href="http://sylvio.nyc/" target="_blank"><img src="../images/sylvio.png"></a>
-       <p class="portfolio-description">Sylvio designs specialty womens wear. I created a site to showcase his collections to the world. The site matches his elegant and edgy approach to design.
-       </br> 
-       </br>
-       </br> 
-       <a href="http://sylvio.nyc/" target="_blank">View Sylvio</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>Sylvio designs specialty womens wear. I created a site to showcase his collections to the world. The site matches his elegant and edgy approach to design.</p>
+          <a href="http://sylvio.nyc/" target="_blank">View Sylvio</a>
+       </div>
+    </li>
 
-    <div class="six columns">
-      <div class="portfolio">
+    <li>
        <p class="title">Ian Moubayed | <span class="descriptor">Portfolio</span></p>
        <a href="http://ianmoubayed.com/" target="_blank"><img src="../images/Ian.jpg"></a>
-       <p class="portfolio-description">Ian Moubayed's is a filmmaker whose work is intimate and moving. I designed and built his site to match the clear, emotional impact of his films.</br>
-       </br> 
-       </br> 
-       <a href="http://ianmoubayed.com/" target="_blank">View Ian Moubayed</a>
-       </p>
-      </div>
-    </div>
+       <div class="portfolio-description">
+          <p>Ian Moubayed's is a filmmaker whose work is intimate and moving. I designed and built his site to match the clear, emotional impact of his films.</p>
+          <a href="http://ianmoubayed.com/" target="_blank">View Ian Moubayed</a>
+       </div>
+    </li>
 
-  </div>
+  </ul>
 
-  <div class="row">
-
-    <div class="six columns">
-      <div class="portfolio">
-       <p class="title">Project 2x1 | <span class="descriptor">Media</span></p>
-       <a href="http://project2x1.com/" target="_blank"><img src="../images/project2x1new.jpg"></a>
-       <p class="portfolio-description">Project 2x1 is a documentary film that needed an interactive website to complement its launch. I designed and built a site that allowed the film’s viewers to truly immerse themselves in the experience and environement of the film’s subject.  
-       </br> 
-       </br>
-       </br> 
-       <a href="http://project2x1.com/" target="_blank">View Project 2x1</a>
-       </p>
-      </div>
-    </div>
-
-    <div class="six columns">
-      <div class="portfolio">
-       <p class="title">Sunwheel Energy | <span class="descriptor">Business</span></p>
-       <a href="http://sunwheelenergy.com/" target="_blank"><img src="../images/sunwheel.png"></a>
-       <p class="portfolio-description">Sunwheel energy is a solar energy and sustainability company. I assisted in crafting the user experience and lead development for the project. Project lead and designer: <a href="http://rorispivey.com/" target="_blank">Rori Spivey</a>
-       </br> 
-       </br> 
-       </br> 
-       <a href="http://sunwheelenergy.com/" target="_blank">View Sunwheel Energy</a>
-       </p>
-      </div>
-    </div>
-
-  </div>
-
+ 
 </div>
 
 <!-- FOOTER -->
