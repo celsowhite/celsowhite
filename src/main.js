607386d882c81ea8c2d732c28870d19412750015
celsowhite <celso.white13@gmail.com>
2018-08-31 08:46:34 -0400

    Add google analytics tracking.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-36564362-1',
+  router
+})
 
 Vue.config.productionTip = false
 
