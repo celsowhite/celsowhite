4d47c8d84e51644c955b77cfc0877d1df6f2b309
celsowhite <celso.white13@gmail.com>
2019-07-16 10:59:39 -0400

    Wrap related websites grid in the main content area so it can transition in a similar fashion.

--- src/components/pages/SingleWebsite.vue
+++ src/components/pages/SingleWebsite.vue
@@ -1,46 +1,48 @@
 <template>
   <div v-if="activeWebsite">
     <PageHeader :title="activeWebsite.title" :excerpt="activeWebsite.excerpt" />
-    <div class="main_content wysiwyg">
-      <div class="panel">
-        <div class="small_container">
-          <video autoplay muted playsinline loop v-if="activeWebsite.video">
-            <source :src="activeWebsite.video" type="video/mp4" />
-          </video>
-          <div v-html="activeWebsite.content"></div>
-          <ul class="credit_list no_bullets">
-            <CreditList :credits="activeWebsite.credits" />
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="panel padding_bottom" v-if="relatedWebsites.length > 0">
-      <div class="large_container">
-        <h3>More {{ primaryCategory }}</h3>
-        <div class="row">
-          <div
-            v-for="relatedWebsite in relatedWebsites"
-            :key="relatedWebsite.title"
-            class="column_1_3"
-          >
-            <ThumbnailCard
-              :title="relatedWebsite.title"
-              :isOverlayed="true"
-              :image="relatedWebsite.featuredImageMedium"
-              :imageFocus="relatedWebsite.featuredImageFocus"
-              :link="'/websites/' + relatedWebsite.slug"
-            />
+    <div class="main_content">
+      <div class="wysiwyg">
+        <div class="panel">
+          <div class="small_container">
+            <video autoplay muted playsinline loop v-if="activeWebsite.video">
+              <source :src="activeWebsite.video" type="video/mp4" />
+            </video>
+            <div v-html="activeWebsite.content"></div>
+            <ul class="credit_list no_bullets">
+              <CreditList :credits="activeWebsite.credits" />
+            </ul>
           </div>
         </div>
       </div>
-    </div>
-    <div class="panel padding_bottom">
-      <div class="large_container text_alignment_center">
-        <h3>
-          <router-link to="/websites" class="featured_link black"
-            >View All Websites</router-link
-          >
-        </h3>
+      <div class="panel padding_bottom" v-if="relatedWebsites.length > 0">
+        <div class="large_container">
+          <h3>More {{ primaryCategory }}</h3>
+          <div class="row">
+            <div
+              v-for="relatedWebsite in relatedWebsites"
+              :key="relatedWebsite.title"
+              class="column_1_3"
+            >
+              <ThumbnailCard
+                :title="relatedWebsite.title"
+                :isOverlayed="true"
+                :image="relatedWebsite.featuredImageMedium"
+                :imageFocus="relatedWebsite.featuredImageFocus"
+                :link="'/websites/' + relatedWebsite.slug"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="panel padding_bottom">
+        <div class="large_container text_alignment_center">
+          <h3>
+            <router-link to="/websites" class="featured_link black"
+              >View All Websites</router-link
+            >
+          </h3>
+        </div>
       </div>
     </div>
   </div>
